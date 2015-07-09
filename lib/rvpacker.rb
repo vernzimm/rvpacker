c35adb63b26dc230e236fa69ac88a5a106f477af
Rachel Wall <solistra@gmx.com>
2015-07-09 18:20:54 -0400

    Move Rvpacker methods from bin/rvpacker to rvpacker.rb.

--- lib/rvpacker.rb
+++ lib/rvpacker.rb
@@ -4,3 +4,19 @@ require 'rvpacker/rgss/psych_mods'
 require 'rvpacker/rgss/serialize'
 require 'rvpacker/rpg'
 require 'rvpacker/version'
+
+module Rvpacker
+  def self.project_type_for(dir)
+    case File.basename(Dir["#{File.expand_path(dir)}/Game.r?proj*"][0] || '.')
+    when 'Game.rxproj'  then 'xp'
+    when 'Game.rvproj'  then 'vx'
+    when 'Game.rvproj2' then 'ace'
+    else
+      'ace'
+    end
+  end
+  
+  def self.valid_project?(dir)
+    !Dir["#{File.expand_path(dir)}/Game.r?proj*"].empty?
+  end
+end
