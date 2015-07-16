7204087e729606a45ffad0d935f7e9130e4e00bb
Rachel Wall <solistra@gmx.com>
2015-07-15 22:19:47 -0400

    Do not sort files when automatically requiring them.

--- lib/rvpacker.rb
+++ lib/rvpacker.rb
@@ -1,5 +1,5 @@
 # Automatically require all `rvpacker` Ruby files in 'lib'.
-Dir[File.expand_path('../**/*.rb', __FILE__)].sort.each(&method(:require))
+Dir[File.expand_path('../**/*.rb', __FILE__)].each(&method(:require))
 
 # `rvpacker` packs and unpacks binary RPG Maker project data to and from YAML
 # so that it can be version-controlled and collaborated on.
