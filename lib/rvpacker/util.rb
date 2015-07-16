b74bcf6a4fda1dae227bcf109e5cdadffbbd66f5
Rachel Wall <solistra@gmx.com>
2015-07-16 04:58:37 -0400

    Split long lines.

--- lib/rvpacker/util.rb
+++ lib/rvpacker/util.rb
@@ -24,8 +24,8 @@ module Rvpacker
   # Defines various utility methods for use by `rvpacker`.
   module Util
     # @param directory [String] the directory to obtain the project type for
-    # @return [:xp, :vx, :ace, nil] the project type of the given directory if it
-    #   could be determined, `nil` otherwise
+    # @return [:xp, :vx, :ace, nil] the project type of the given directory if
+    #   it could be determined, `nil` otherwise
     def self.project_type_for(directory)
       case Dir["#{File.expand_path(directory)}/Game.r?proj*"][0]
       when /Game\.rxproj$/  then :xp
@@ -35,8 +35,8 @@ module Rvpacker
     end
 
     # @param directory [String] the directory to test the validity of
-    # @return [Boolean] `true` if the directory is the root of a valid RPG Maker
-    #   project, `false` otherwise
+    # @return [Boolean] `true` if the directory is the root of a valid RPG
+    #   Maker project, `false` otherwise
     def self.valid_project?(directory)
       file = Dir["#{File.expand_path(directory)}/Game.r?proj*"][0]
       file =~ /Game\.r(?:xproj|vproj2?)$/ ? true : false
